9a9d89c19b9d4ca08cb3925040d8e65415e5cb64
Komal0patel <komal.1620patel@gmail.com>
2026-09-02 06:56:24 +0530

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   server: {
     // SPA fallback: serve index.html for all unknown paths so React Router handles routing
     historyApiFallback: true,
